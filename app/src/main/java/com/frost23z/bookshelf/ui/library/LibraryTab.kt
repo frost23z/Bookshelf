b75ad0fde2f78e3a35a33de18f4372307c5f86a4
Shaiekh Zayed Bin Hasan <80541095+frost23z@users.noreply.github.com>
2024-10-21 15:04:20 +0600

    feat: Update LibraryTab with animated icon

--- app/src/main/java/com/frost23z/bookshelf/ui/library/LibraryTab.kt
+++ app/src/main/java/com/frost23z/bookshelf/ui/library/LibraryTab.kt
@@ -1,25 +1,31 @@
 package com.frost23z.bookshelf.ui.library
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.LibraryBooks
+import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
+import com.frost23z.bookshelf.R
 
 object LibraryTab : Tab {
+    @OptIn(ExperimentalAnimationGraphicsApi::class)
     override val options: TabOptions
         @Composable get() {
             LocalTabNavigator.current.current.key == key
             return TabOptions(
                 index = 0u,
                 title = "Library",
-                icon = rememberVectorPainter(Icons.AutoMirrored.Filled.LibraryBooks),
+                icon = rememberAnimatedVectorPainter(
+                    animatedImageVector = AnimatedImageVector.animatedVectorResource(R.drawable.anim_library_select),
+                    atEnd = LocalTabNavigator.current.current.key == key
+                )
             )
         }
 
