4b2842eb4132fd100cff4a26fc6212c86a14bf89
Shaiekh Zayed Bin Hasan <80541095+frost23z@users.noreply.github.com>
2025-01-16 03:09:12 +0600

    feat(ui): add constants for Icon sizes and Padding, and create EmptyScreen composable

---
+++ app/src/main/java/com/frost23z/bookshelf/ui/core/constants/Padding.kt
@@ -0,0 +1,13 @@
+package com.frost23z.bookshelf.ui.core.constants
+
+import androidx.compose.ui.unit.dp
+
+object Padding {
+	val XXSmall = 4.dp
+	val XSmall = 8.dp
+	val Small = 12.dp
+	val Medium = 16.dp
+	val Large = 24.dp
+	val XLarge = 32.dp
+	val XXLarge = 64.dp
+}
