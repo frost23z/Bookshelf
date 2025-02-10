06c91113b9f210c545c97a898fb6fa9f89beee85
Shaiekh Zayed Bin Hasan <80541095+frost23z@users.noreply.github.com>
2025-02-11 00:08:42 +0600

    feat(ui): move DatePickerFor enum to its own file for better organization

--- app/src/main/java/com/frost23z/bookshelf/ui/addedit/components/AddEditScreenState.kt
+++ app/src/main/java/com/frost23z/bookshelf/ui/addedit/components/AddEditScreenState.kt
@@ -3,14 +3,8 @@ package com.frost23z.bookshelf.ui.addedit.components
 import androidx.compose.runtime.Immutable
 import com.frost23z.bookshelf.domain.models.AcquisitionType
 import com.frost23z.bookshelf.domain.models.Books
+import com.frost23z.bookshelf.ui.addedit.models.DatePickerFor
 import com.frost23z.bookshelf.ui.core.models.booksInit
-
-enum class DatePickerFor {
-	PUBLICATION_DATE,
-	ACQUIRED_DATE,
-	START_READING_DATE,
-	FINISHED_READING_DATE
-}
 
 @Immutable
 data class AddEditScreenState(
---
+++ app/src/main/java/com/frost23z/bookshelf/ui/addedit/models/DatePickerFor.kt
@@ -0,0 +1,8 @@
+package com.frost23z.bookshelf.ui.addedit.models
+
+enum class DatePickerFor {
+	PUBLICATION_DATE,
+	ACQUIRED_DATE,
+	START_READING_DATE,
+	FINISHED_READING_DATE
+}
