edf98d8c3779c61714efaa7e3b7ab7616f3f6638
Shaiekh Zayed Bin Hasan <80541095+frost23z@users.noreply.github.com>
2025-02-15 02:35:54 +0600

    feat(ui): mark LibraryScreenState as Immutable for performance optimization

--- app/src/main/java/com/frost23z/bookshelf/ui/library/components/LibraryScreenState.kt
+++ app/src/main/java/com/frost23z/bookshelf/ui/library/components/LibraryScreenState.kt
@@ -1,7 +1,9 @@
 package com.frost23z.bookshelf.ui.library.components
 
+import androidx.compose.runtime.Immutable
 import com.frost23z.bookshelf.domain.models.LibraryBooks
 
+@Immutable
 data class LibraryScreenState(
 	val isLoading: Boolean = true,
 	val library: List<LibraryBooks> = emptyList()
