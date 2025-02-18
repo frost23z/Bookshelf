74fad1e9217cb5b9f60a32bdfa5bd7549970f853
Shaiekh Zayed Bin Hasan <80541095+frost23z@users.noreply.github.com>
2025-02-18 23:23:53 +0600

    feat(di): add AppDatabase initialization with necessary adapters in AppModule

--- app/src/main/java/com/frost23z/bookshelf/di/AppModule.kt
+++ app/src/main/java/com/frost23z/bookshelf/di/AppModule.kt
@@ -1,5 +1,58 @@
 package com.frost23z.bookshelf.di
 
+import androidx.sqlite.db.SupportSQLiteDatabase
+import app.cash.sqldelight.EnumColumnAdapter
+import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import com.frost23z.bookshelf.data.AppDatabase
+import com.frost23z.bookshelf.data.adapters.LocalDateAdapter
+import com.frost23z.bookshelf.data.adapters.LocalDateListAdapter
+import com.frost23z.bookshelf.data.adapters.QuoteSetAdapter
+import com.frost23z.bookshelf.data.adapters.UriAdapter
+import com.frost23z.bookshelf.domain.models.AcquiredFrom
+import com.frost23z.bookshelf.domain.models.Books
+import com.frost23z.bookshelf.domain.models.MapperBooksContributors
+import com.frost23z.bookshelf.domain.models.Series
 import org.koin.dsl.module
 
-val appModule = module {}
+val appModule = module {
+	single {
+		AndroidSqliteDriver(
+			schema = AppDatabase.Schema,
+			context = get(),
+			name = "bookshelf.db",
+			callback = object : AndroidSqliteDriver.Callback(AppDatabase.Schema) {
+				override fun onOpen(db: SupportSQLiteDatabase) {
+					db.setForeignKeyConstraintsEnabled(true)
+				}
+			}
+		)
+	}
+	single {
+		AppDatabase(
+			driver = get(),
+			acquiredFromAdapter = AcquiredFrom.Adapter(
+				acquisitionTypeAdapter = EnumColumnAdapter()
+			),
+			booksAdapter = Books.Adapter(
+				coverUriAdapter = UriAdapter,
+				publicationDateAdapter = LocalDateAdapter,
+				formatAdapter = EnumColumnAdapter(),
+				acquiredDateAdapter = LocalDateAdapter,
+				readStatusAdapter = EnumColumnAdapter(),
+				startReadingDateAdapter = LocalDateAdapter,
+				finishedReadingDateAdapter = LocalDateAdapter,
+				additionalReadingDatesAdapter = LocalDateListAdapter,
+				loanAdapter = EnumColumnAdapter(),
+				loanedDateAdapter = LocalDateAdapter,
+				expectedReturnDateAdapter = LocalDateAdapter,
+				quotesAdapter = QuoteSetAdapter
+			),
+			mapperBooksContributorsAdapter = MapperBooksContributors.Adapter(
+				roleAdapter = EnumColumnAdapter()
+			),
+			seriesAdapter = Series.Adapter(
+				firstPublicationDateAdapter = LocalDateAdapter
+			)
+		)
+	}
+}
