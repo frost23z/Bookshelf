12dc80ecdcaaa611046634eb65440e90d45ecabe
Shaiekh Zayed Bin Hasan <80541095+frost23z@users.noreply.github.com>
2025-02-25 00:03:00 +0600

    feat(di): add LibraryRepository to dependency injection in DomainModule

--- app/src/main/java/com/frost23z/bookshelf/di/DomainModule.kt
+++ app/src/main/java/com/frost23z/bookshelf/di/DomainModule.kt
@@ -1,9 +1,12 @@
 package com.frost23z.bookshelf.di
 
 import com.frost23z.bookshelf.data.repositories.AddEditRepositoryImpl
+import com.frost23z.bookshelf.data.repositories.LibraryRepositoryImpl
 import com.frost23z.bookshelf.domain.repositories.AddEditRepository
+import com.frost23z.bookshelf.domain.repositories.LibraryRepository
 import org.koin.dsl.module
 
 val domainModule = module {
 	single<AddEditRepository> { AddEditRepositoryImpl(db = get()) }
+	single<LibraryRepository> { LibraryRepositoryImpl(db = get()) }
 }
