6c833df6fba67ea91bc2275bbd456d12d51d9aa7
Shaiekh Zayed Bin Hasan <80541095+frost23z@users.noreply.github.com>
2025-03-13 00:40:01 +0600

    feat(navigation): introduce Destination interface for navigation structure and remove NavBarDestinations

---
+++ app/src/main/java/com/frost23z/bookshelf/ui/core/navigation/Destination.kt
@@ -0,0 +1,47 @@
+package com.frost23z.bookshelf.ui.core.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed interface Destination {
+	@Serializable
+	data object LibraryGraph : Destination
+
+	@Serializable
+	data object Library : Destination
+
+	@Serializable
+	data object Detail : Destination
+
+	@Serializable
+	data object ReadingGraph : Destination
+
+	@Serializable
+	data object Reading : Destination
+
+	@Serializable
+	data object AddEditGraph : Destination
+
+	@Serializable
+	data object AddEdit : Destination
+
+	@Serializable
+	data object LentGraph : Destination
+
+	@Serializable
+	data object Lent : Destination
+
+	@Serializable
+	data object MoreGraph : Destination
+
+	@Serializable
+	data object More : Destination
+
+	companion object {
+		val navBarItems = listOf(
+			Library,
+			Reading,
+			Lent,
+			More
+		)
+	}
+}
