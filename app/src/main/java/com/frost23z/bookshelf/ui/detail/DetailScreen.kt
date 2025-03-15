1cc8afeae0b8925cfe7e9bcbc6654f5edb7a4ac2
Shaiekh Zayed Bin Hasan <80541095+frost23z@users.noreply.github.com>
2025-03-15 22:38:14 +0600

    feat(detail): enhance DetailScreen with back navigation and display book details

--- app/src/main/java/com/frost23z/bookshelf/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/frost23z/bookshelf/ui/detail/DetailScreen.kt
@@ -1,13 +1,22 @@
-package com.frost23z.bookshelf.ui.addedit
+package com.frost23z.bookshelf.ui.detail
 
+import android.util.Log
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.frost23z.bookshelf.ui.core.components.IconButton
 import com.frost23z.bookshelf.ui.core.components.Screen
+import com.frost23z.bookshelf.ui.detail.components.DetailScreen
+import com.frost23z.bookshelf.ui.detail.components.DetailScreenEvent
 import com.frost23z.bookshelf.ui.detail.components.DetailScreenModel
 import kotlinx.serialization.Serializable
 import org.koin.compose.koinInject
@@ -17,14 +26,22 @@ class DetailScreen : Screen {
 	@OptIn(ExperimentalMaterial3Api::class)
 	@Composable
 	override fun Content() {
+		Log.d("Detail", "Detailjhjjj")
 		val screenModel = koinInject<DetailScreenModel>()
 		val state by screenModel.state.collectAsStateWithLifecycle()
 
 		Scaffold(
 			topBar = {
-				TopAppBar(title = { Text("Detail") })
+				TopAppBar(title = { Text("Detail") }, navigationIcon = {
+					IconButton(icon = Icons.AutoMirrored.Outlined.ArrowBack, onClick = {
+						screenModel.onEvent(
+							DetailScreenEvent.Back
+						)
+					})
+				})
 			}
 		) { innerPadding ->
+			DetailScreen(state = state, onEvent = screenModel::onEvent, modifier = Modifier.padding(innerPadding))
 		}
 	}
 }
--- app/src/main/java/com/frost23z/bookshelf/ui/detail/components/DetailScreen.kt
+++ app/src/main/java/com/frost23z/bookshelf/ui/detail/components/DetailScreen.kt
@@ -1,7 +1,12 @@
 package com.frost23z.bookshelf.ui.detail.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun DetailScreen(
@@ -9,5 +14,13 @@ fun DetailScreen(
 	onEvent: (DetailScreenEvent) -> Unit,
 	modifier: Modifier = Modifier
 ) {
-	// TODO: Implement DetailScreen
+	Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+		Text(text = state.book.toString())
+	}
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun gf() {
+	DetailScreen(state = DetailScreenState(), onEvent = {})
 }
