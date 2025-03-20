98f2bcff4a0c14f0803179ef77bf7d29c8ec6e57
Shaiekh Zayed Bin Hasan <80541095+frost23z@users.noreply.github.com>
2025-03-20 23:55:22 +0600

    feat(home): update FloatingActionButton animation for improved visibility transitions

--- app/src/main/java/com/frost23z/bookshelf/ui/home/HomeScreen.kt
+++ app/src/main/java/com/frost23z/bookshelf/ui/home/HomeScreen.kt
@@ -4,12 +4,14 @@ import android.util.Log
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.expandIn
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
 import androidx.compose.animation.graphics.res.animatedVectorResource
 import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
 import androidx.compose.animation.graphics.vector.AnimatedImageVector
+import androidx.compose.animation.shrinkOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
@@ -138,8 +140,8 @@ object HomeScreen : Screen {
 			floatingActionButton = {
 				AnimatedVisibility(
 					visible = showBottomBar,
-					enter = expandHorizontally(),
-					exit = shrinkVertically()
+					enter = expandIn(),
+					exit = shrinkOut()
 				) {
 					FloatingActionButton(
 						onClick = { screenModel.onEvent(HomeScreenEvent.ToggleAddOptionsBottomsheet) },
